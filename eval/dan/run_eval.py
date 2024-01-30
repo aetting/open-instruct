44dccfc34c0dd0dcefd7f380d02ea06c824717a0
Allyson Ettinger <allysone@allenai.org>
2024-01-30 17:13:42 -0500

    accom q only and jailbreaks

--- eval/dan/run_eval.py
+++ eval/dan/run_eval.py
@@ -69,6 +69,7 @@ def judge_compliance(args,model,tokenizer,questions,outputs
 def collect_items(args,datafile):
     examples = []
     components = pd.read_csv(datafile)
+    if args.questions_only: components = components.query('response_idx==0')
     if args.num_instances is not None:
         components = components.sample(args.num_instances, random_state=42)
     if args.questions_only:
@@ -78,7 +79,6 @@ def collect_items(args,datafile):
                 "question": question,
             })
     else:
-        components = components.query('response_idx==0')
         for idx in components.index:
             jailbreak = components.loc[idx,"prompt"]
             question = components.loc[idx,"question"]

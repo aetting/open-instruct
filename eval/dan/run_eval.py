3a1f553281b3192fad782c32b352801d1adf03f2
Allyson Ettinger <allysone@allenai.org>
2024-01-30 16:57:59 -0500

    accom q only and jailbreaks

--- eval/dan/run_eval.py
+++ eval/dan/run_eval.py
@@ -68,7 +68,7 @@ def judge_compliance(args,model,tokenizer,questions,outputs
 
 def collect_items(args,datafile):
     examples = []
-    components = pd.read_csv(datafile)
+    components = pd.read_csv(datafile).query('response_idx==0')
     import pdb; pdb.set_trace()
     if args.num_instances is not None:
         components = components.sample(args.num_instances, random_state=42)

bb7e539297065a830f2a19dec63b13e4c61f82fa
Allyson Ettinger <allysone@allenai.org>
2024-02-05 10:21:05 -0500

    subset for testing

--- eval/dan/judge_compliance.py
+++ eval/dan/judge_compliance.py
@@ -88,7 +88,7 @@ def main(args):
     answer_choice_ids = [tokenizer.encode(" " + answer_choice, add_special_tokens=False)[-1] for answer_choice in choices]
     # answer_choice_ids = None
     pred_tokens, _ = get_next_word_predictions(
-        model, tokenizer, prompts, candidate_token_ids=answer_choice_ids, return_token_predictions=True
+        model, tokenizer, prompts[:15], candidate_token_ids=answer_choice_ids, return_token_predictions=True
     )
     print(pred_tokens)
     complied = [re.match(".*[Y|y]es",resp) is not None for resp in pred_tokens]

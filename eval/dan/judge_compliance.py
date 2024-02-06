eaf5f2da7b90d7e092df464f8ab2e9466d0f0ef0
Allyson Ettinger <allysone@allenai.org>
2024-02-05 19:15:45 -0500

    subsample prompts to test

--- eval/dan/judge_compliance.py
+++ eval/dan/judge_compliance.py
@@ -88,7 +88,7 @@ def main(args):
         prompts.append(prompt)
 
 
-    prompts = prompts[:4]
+    prompts = prompts[:5]
     answer_choice_ids = [tokenizer.encode(" " + answer_choice, add_special_tokens=False)[-1] for answer_choice in choices]
     # answer_choice_ids = None
     pred_tokens, _ = get_next_word_predictions(
@@ -110,11 +110,13 @@ def main(args):
             curr_result["prompt"] = prompt
             curr_result["tok"] = tok
             curr_result["compliance"] = compliance
-            if i % 50 == 0:
+            if i % 2 == 0:
                 print(f"{prompt}\n{tok}\n{compliance}\n\n~~~~\n\n")
             fout.write(json.dumps(curr_result) + "\n")
-    print(complied)
-    print(sum(complied))
+        print(complied)
+        print(sum(complied))
+        fout.write(json.dumps(complied) + "\n")
+        fout.write(json.dumps(sum(complied)) + "\n")
 
 
 
